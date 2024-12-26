327722281afb15bf49b011bd24d383ff5a39b948
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-12-26 13:24:54 +0600

    tag data & user section added

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Tag = () => {
+const Tag = ({data}:{data:string}) => {
   return (
-    <div>Tag</div>
+    <div className='bg-black rounded-md px-2 py-1 shadow-md text-xs
+    text-white'>{data}</div>
   )
 }
 
