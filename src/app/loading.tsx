59fad3d792af777d6730931d35a5af31da6dafe0
Mahmudul-Hassan-Ratul <mahmudulhassanratul.dev@gmail.com>
2024-12-28 01:30:01 +0600

    auth secret added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='text-center text-3xl px-5 py-3'>Loading...</div>
+    <div className='text-center text-3xl px-5 py-4'>Loading...</div>
   )
 }
 
